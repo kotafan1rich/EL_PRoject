458aaed726afd5e05edfe074698d6bf5f3e7b34a
kotafan1rich <lesapond2007@gmail.com>
2024-03-03 19:21:31 +0300

    api.sh changed

--- fastApiEL/docker/api.sh
+++ fastApiEL/docker/api.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-RUN apt-get update && apt-get install -y netcat
-
-
-echo "Waiting for database to be ready..."
-while ! nc -z db 5432; do
-  sleep 1
-done
-
 alembic upgrade head
 
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --reload
