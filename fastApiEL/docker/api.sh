bfe8eaa162f7fbe7d0d7363d8a47b815b3c1a813
kotafan1rich <lesapond2007@gmail.com>
2024-03-03 19:02:04 +0300

    api.sh changed

--- fastApiEL/docker/api.sh
+++ fastApiEL/docker/api.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Waiting for database to be ready..."
+while ! nc -z db 5432; do
+  sleep 1
+done
+
 alembic upgrade head
 
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --reload
