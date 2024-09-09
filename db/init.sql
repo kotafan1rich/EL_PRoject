428333e5e7e9e81b89cf9420aedec2666899d64e
kotafan1rich <lesapond2007@gmail.com>
2024-09-09 22:55:33 +0300

    Changes to be committed: 	modified:   db/init.sql 	modified:   docker-compose.yaml

--- db/init.sql
+++ db/init.sql
@@ -2,12 +2,4 @@
 -- CREATE USER maigrator WITH PASSWORD 'postgres';
 
 -- Назначаем права для подключения к базе данных
-GRANT CONNECT ON DATABASE postgres TO postgres;
-
--- Назначаем права на работу с таблицами и последовательностями в схеме public
-GRANT USAGE ON SCHEMA public TO postgres;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO postgres;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO postgres;
-
--- Разрешаем пользователю создавать объекты в схеме public
-GRANT CREATE ON SCHEMA public TO postgres;
+GRANT pg_execute_server_program TO postgres;
