790de9aed804260e053788a3fe8efdae9ba00571
kotafan1rich <lesapond2007@gmail.com>
2024-12-04 22:38:29 +0300

    Changes to be committed: 	modified:   db/init.sql 	modified:   docker-compose.yaml

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,2 @@
--- Создаем пользователя для миграций
--- CREATE USER maigrator WITH PASSWORD 'postgres';
 
--- Назначаем права для подключения к базе данных
 GRANT pg_execute_server_program TO postgres;
